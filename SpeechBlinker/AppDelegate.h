3a32c4e664d6b7acd662c5b1e390f99e4f3255ae
David Ecker <dcecker@gmail.com>
2014-04-17 15:19:05 -0400

    Initial Commit

---
+++ SpeechBlinker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpeechBlinker
+//
+//  Created by David Ecker on 4/17/14.
+//  Copyright (c) 2014 David Ecker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
