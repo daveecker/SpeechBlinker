8ce91641b577febd8e73c727c9070a7e2767fe33
David Ecker <dcecker@gmail.com>
2014-04-21 15:15:30 -0400

    initial commit

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpeechBlinker
+//
+//  Created by David Ecker on 4/17/14.
+//  Copyright (c) 2014 David Ecker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
